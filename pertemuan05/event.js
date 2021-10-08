e3fa1095d719adbce280f160d6f1846e08ff2499
rafivalli261 <valliansyah@gmail.com>
2021-10-08 10:47:27 +0700

    lanjut belajar jquery

--- pertemuan05/event.js
+++ pertemuan05/event.js
@@ -5,4 +5,33 @@ $(document).ready(function () {
 		$(this).hide();
 		//selector judul class
 	});
+	$("input").focus(function () {
+		$(this).css("background-color", "yellow");
+	});
+	$("input").blur(function () {
+		$(this).css("background-color", "transparent");
+	});
+	// $(".coba_mouseenter").mouseenter(function () {
+	// 	alert("MouseEnter nih boss, sengggol dong");
+	// }); //bahaya cuy
+	$(".mouse").on({
+		mouseenter: function () {
+			$(this).css("background-color", "salmon");
+		},
+		mouseleave: function () {
+			$(this).css("background-color", "lightskyblue");
+		},
+		click: function () {
+			$(this).css("background-color", "yellow");
+		},
+		mousedown: function () {
+			$(this).css("background-color", "silver");
+		},
+		// mouseup: function () {
+		// 	alert("lepas");
+		// },
+		// mousemove: function () {
+		// 	$(this).css("background-color", "pink");
+		// },
+	});
 });
