8ac5830e6307eca29ae945721bc579725f78f9e6
rafivalli261 <valliansyah@gmail.com>
2021-10-12 22:54:06 +0700

    belajar manipulasi html dengan dom

---
+++ pertemuan04/script1.js
@@ -0,0 +1,5 @@
+//belajar manipulasi dengan javascript biasa dulu
+let harus = document.getElementById("coba1");
+harus.style.backgroundColor = "lightskyblue";
+harus.style.fontFamily = "Halvetica, sans-serif";
+harus.innerHTML = "Sistem Periodik Unsur memuat semua unsur yang telah didokumentasikan oleh para peneliti. ";
