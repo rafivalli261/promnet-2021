2415a7fc21c3f4e5de5311cf11f74ff0ab9a45fc
rafivalli261 <valliansyah@gmail.com>
2021-10-13 22:55:17 +0700

    lanjut belajar DOM

--- pertemuan04/script1.js
+++ pertemuan04/script1.js
@@ -3,3 +3,13 @@ let harus = document.getElementById("coba1");
 harus.style.backgroundColor = "lightskyblue";
 harus.style.fontFamily = "Halvetica, sans-serif";
 harus.innerHTML = "Sistem Periodik Unsur memuat semua unsur yang telah didokumentasikan oleh para peneliti. ";
+let saat = document.getElementById("waktu");
+saat.innerHTML = new Date();
+let tekan = document.getElementById("tombol");
+// tidak bisa, harus panggil fungsi
+//tekan.onclick = Date();
+function tampil_tanggal() {
+	let tanggal = new Date().toDateString();
+	tekan.innerHTML = tanggal;
+	console.log(typeof tekan);
+}
