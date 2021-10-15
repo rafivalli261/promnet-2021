ac798c02720ae8e5640d57088b5f0b7b56f31040
rafivalli261 <valliansyah@gmail.com>
2021-10-15 11:22:57 +0700

    kelas libur, mengerjakan lkm 2 tentang jquery

---
+++ pertemuan06/lkm2.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	$("h1").css("background-color", "salmon");
+	$("h2, p").css("font-family", "Halvetica, sans-serif");
+	$("#p2").hide();
+	$("#btn1").click(function () {
+		//$("p").hide("slow"); //menyembunyikan elemen, parameter berfungsi sebagai waktu untuk melakukan aksi, dalam milisekon, bisa juga string slow , fast
+
+		//$("p").toggle(2000); //menyembunyikan dan menampilkan elemen, mirip dengan hide
+		// $("p").hide("slow", function () {
+
+		// 	//dalam event, kita bisa menambahkan parameter callback, yakni parameter yang memungkinkan kita untuk memanggil fungsi lain setelah event selesai dilakukan
+		// 	alert("Konten telah diamankan");
+		// });
+
+		// $("#p1").hide("slow", function () {
+		// 	$("#p2").show("slow");
+		// });
+		$("#p1").slideDown(2000).hide();
+		$("#p2").show(2000);
+	});
+	//$("p").css("background-color", "lightskyblue").slideUp(2000).slideDown(2000);
+});
